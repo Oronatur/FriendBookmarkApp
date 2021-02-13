845c63d18dce0584a12b6ed805fa932b16294e99
Oronatur <Oronatur@users.noreply.github.com>
2021-02-14 00:29:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,51 +11,61 @@ m.onExecuted = function(e,d)
                     {
                         var list = JSON.parse(d);
                         var listContainer = document.createElement('div');
+                        listContainer.setAttribute("class","FullWidth");
                         var numberOfListItems = list.length;
                     
                         document.getElementById('jslist').appendChild(listContainer);
                         
                         for (i = 0; i < numberOfListItems; i++)
                             {
+                                
                                 listElement = document.createElement('div');
                                 listElement.setAttribute("id", "entry" + list[i].ID);
+                                listElement.setAttribute("class", "listElement")
                                 
-                                listUrl = document.createElement('div');
-                                listUrl.setAttribute("class","HContent30 BorderRight Ellipsis FloatLeft");
-                                listUrl.setAttribute("id", "Url"+list[i].ID);
+                                listUrl = document.createElement('button');
+                                listUrl.setAttribute("class","HContent70 BorderRight Ellipsis FloatLeft");
+                                listUrl.setAttribute("id", "Url" + i);
+                                listUrl.setAttribute("onClick", "goTo(this.value)");
+                                listUrl.setAttribute("title", "Go to site");
+                                listUrl.setAttribute("value", list[i].Url);
                                 listUrl.innerHTML = list[i].Url;
                                 listElement.appendChild(listUrl);
                                 
                                 listEdit = document.createElement('button');
-                                listEdit.setAttribute("class","HContent10 Ellipsis FloatLeft");
+                                listEdit.setAttribute("class","HContent Ellipsis FloatLeft IconSmall fas fa-edit");
                                 listEdit.setAttribute("onClick","save(this.value);")
-                                listEdit.setAttribute("id","editButton" + list[i].ID);
+                                listEdit.setAttribute("id","editButton" + i);
+                                listEdit.setAttribute("title", "Edit");
                                 listEdit.setAttribute("value", list[i].ID);
-                                listEdit.innerHTML = "E";
+                                listEdit.innerHTML = "";
                                 listElement.appendChild(listEdit);
                                 
                                 listDelete = document.createElement('button');
-                                listDelete.setAttribute("class","HContent10 Ellipsis FloatLeft");
-                                listDelete.setAttribute("id","deleteButton" + list[i].ID);
+                                listDelete.setAttribute("class","HContent Ellipsis FloatLeft IconSmall fa fa-trash");
+                                listDelete.setAttribute("id","deleteButton" + i);
                                 listDelete.setAttribute("onClick","remove(value);")
+                                listDelete.setAttribute("title", "Delete");
                                 listDelete.setAttribute("value", list[i].ID);
-                                listDelete.innerHTML = "D";
+                                listDelete.innerHTML = "";
                                 listElement.appendChild(listDelete);
                                 
                                 listCopy = document.createElement('button');
-                                listCopy.setAttribute("class","HContent10 Ellipsis FloatLeft");
+                                listCopy.setAttribute("class","HContent Ellipsis FloatLeft IconSmall far fa-copy");
                                 listCopy.setAttribute("onClick","copy(this.value);")
-                                listCopy.setAttribute("id","copyButton" + list[i].ID);
+                                listCopy.setAttribute("id","copyButton" + i);
+                                listCopy.setAttribute("title", "Copy to clipboard");
                                 listCopy.setAttribute("value", list[i].Url);
-                                listCopy.innerHTML = "C";
+                                listCopy.innerHTML = "";
                                 listElement.appendChild(listCopy);
                                 
                                 listGoTo = document.createElement('button');
-                                listGoTo.setAttribute("class","HContent10 Ellipsis FloatLeft");
+                                listGoTo.setAttribute("class","HContent Ellipsis FloatLeft IconSmall fa fa-external-link");
                                 listGoTo.setAttribute("onClick", "goTo(this.value)");
-                                listGoTo.setAttribute("id", "goToButton" + list[i].ID)
+                                listGoTo.setAttribute("id", "goToButton" + i);
+                                listGoTo.setAttribute("title", "Go to site");
                                 listGoTo.setAttribute("value", list[i].Url);
-                                listGoTo.innerHTML = "G";
+                                listGoTo.innerHTML = "";
                                 listElement.appendChild(listGoTo);
                                 
                                 listContainer.appendChild(listElement);
@@ -68,10 +78,10 @@ m.onExecuted = function(e,d)
             {
                 //no need to do anything, will only happen on first exec.
             }
-        else
+        else if(e == "initerror")
             {
                 console.log(d)
-                document.getElementById('jslist').textContent = "Error initializing Bookmarks. Please restart the app or contact a system administrator if the problem persists.";
+                document.getElementById('jslist').textContent = "Error initializing Bookmarks. Please restart the app.";
             }
             
     }      
@@ -79,10 +89,22 @@ m.onExecuted = function(e,d)
 m.execute('init');
 
 //called when editing an existing bookmark or creating a new one.
+//value = id
 function save(value)
 {
     var url = prompt("Please enter URL");
-    if (url != null && value != null)
+    
+    if (url === null)
+        {
+            return;
+            //if they hit cancel, do nothing.
+        }
+    else if (url == "")
+        {
+            alert("Bookmark cannot be empty.")
+            var url = prompt("Please enter URL");
+        }
+    else if (url != null && value != null)
         {
             m.execute('save', {id:value,url:url});
             //set the correct values when editing a bookmark without reloading the page.
@@ -91,7 +113,7 @@ function save(value)
             document.getElementById("copyButton" + value).value = url;
             urlShow.innerHTML = url;
         }
-    else if(url != null && value == null)
+    else if (url != null && value == null)
         {
             m.execute('save', {id:null,url:url})
             //should probably just use some ajax for this but good enough for now. Refreshes page after adding new bookmark.
@@ -104,6 +126,7 @@ function save(value)
         }
 }
 
+//value = id
 function remove(value)
 {
     m.onExecuted = function(e,d)
@@ -118,11 +141,22 @@ function remove(value)
                     location.reload();
                 }
         }        
-                
-    m.execute('delete', {id:value});   
+ 
+    var confirm  = window.confirm("Delete bookmark?");
+    
+    if(confirm)
+        {
+            m.execute('delete', {id:value});   
+        }
+    else
+        {
+            return;
+        }
+            
 }
 
 
+//value = url
 function copy(value)
 {
     var copy = document.createElement('textarea');
@@ -134,6 +168,7 @@ function copy(value)
 }
 
 //opens bookmark in a new tab. Uses the default browser, not the friend browser.
+//value = url
 function goTo(value)
 {
     var newTab = window.open('https://' + value, '_blank');
